9e3681e6dabd735ecef0339e76df4bd74620d566
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-07-11 22:49:43 -0300

    feat: Add Header / Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import { Route, Routes } from "react-router-dom";
+import { Header, Footer } from "./components";
 import { SignIn, SignUp, Home, Product, Cart, CheckOut } from "./pages/index";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/product/:id" element={<Product />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/cart" element={<Cart />} />
-      <Route path="/checkout" element={<CheckOut />} />
-    </Routes>
+    <>
+      <Header />
+      <Footer />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<CheckOut />} />
+      </Routes>
+    </>
   );
 }
 
