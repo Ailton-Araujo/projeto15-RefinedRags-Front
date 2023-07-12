742c7a0856943114fcc6418adfa98e8961c636ab
Ailton Araujo <ailton.correia.araujo@gmail.com>
2023-07-11 22:22:54 -0300

    build: App Structure

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import SignIn from "./SignIn";
+import SignUp from "./SignUp";
+import Home from "./Home";
+import Product from "./Product";
+import Cart from "./Cart";
+import CheckOut from "./CheckOut";
+
+export { SignIn, SignUp, Home, Product, Cart, CheckOut };
