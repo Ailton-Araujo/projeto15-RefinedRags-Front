0b095ed559aa65052a1cbe743efc17d56f7d8f16
MarcusDantas <marvinsd@gmail.com>
2023-07-14 15:51:07 -0300

    feat: add product page with form to inform quantity and size for order

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,1 +1,91 @@
-export default function Product() {}
+import useProducts from "../../hooks/useProducts";
+import { useParams } from "react-router-dom";
+import { useEffect, useState, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import { Container, BackButton } from "./style";
+import { RotatingLines } from "react-loader-spinner";
+
+export default function Product() {
+  const { products } = useProducts();
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const [inventory, setInventory] = useState([]);
+  const [quantity, setQuantity] = useState(1);
+  const sizeRef = useRef(null);
+  const navigate = useNavigate();
+
+  function addToCart() {
+    //Colocar no carrinho
+  }
+
+  function handleForm(event) {
+    event.preventDefault();
+
+    const data = {
+      size: sizeRef.current.value,
+      quantity: quantity,
+    };
+
+    if (data.quantity <= 0) {
+      return alert("Insert valid quantity.");
+    }
+
+    if (inventory.indexOf(data.size) === -1) {
+      return alert("Choose size.");
+    }
+
+    addToCart();
+  }
+
+  useEffect(() => {
+    const foundProduct = products.find((item) => item.productId === Number(id));
+    if (foundProduct) {
+      setProduct(foundProduct);
+      const filteredSizes = Object.entries(foundProduct.inventory)
+        .filter(([size, quantity]) => quantity !== 0)
+        .map(([size]) => size);
+      setInventory(filteredSizes);
+    }
+  }, [products]);
+
+  return (
+    <Container>
+      {products.length === 0 ? (
+          <RotatingLines strokeColor="#000000" strokeWidth="4" width="80" />
+        ) : (
+        <>
+          <img src={product.img} alt={product.name} />
+          <div>
+            <h2>{product.name}</h2>
+            <h3>{"$" + product.price}</h3>
+            <form onSubmit={(event) => handleForm(event)}>
+              <select ref={sizeRef}>
+                <option>Select size</option>
+                {product.type === "sneakers"
+                  ? inventory.map((size, index) => {
+                      return (
+                        <option value={size} key={index}>
+                          {size.replace("s", "").toUpperCase()}
+                        </option>
+                      );
+                    })
+                  : inventory.map((size, index) => {
+                      return (
+                        <option value={size} key={index}>
+                          {size.toUpperCase()}
+                        </option>
+                      );
+                    })}
+              </select>
+              <input type="number" min="1" value={quantity} onChange={(event) => setQuantity(event.target.value)} />
+              <button>Add To Cart</button>
+            </form>
+            <h3>Product Details</h3>
+            <p>{product.description}</p>
+          </div>
+          <BackButton onClick={() => navigate("/")} />
+        </>
+      )}
+    </Container>
+  );
+}
---
+++ src/pages/Product/style.js
@@ -0,0 +1,137 @@
+import styled from "styled-components";
+import { BsFillArrowLeftSquareFill } from "react-icons/bs";
+
+export const Container = styled.div`
+  background-color: #ffffff;
+  display: flex;
+  gap: 50px;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  border-radius: 10px;
+  width: 100%;
+  max-width: 800px;
+  margin: 200px auto;
+  font-family: Arial, Helvetica, sans-serif;
+
+  img {
+    width: 450px;
+    border-radius: 10px;
+  }
+
+  form {
+    display: block;
+  }
+
+  h2 {
+    font-size: 30px;
+    margin: 10px 0;
+    line-height: 40px;
+    font-weight: 700;
+  }
+
+  h3 {
+    color: #301185;
+    font-weight: 500;
+    margin-bottom: 15px;
+    font-size: 20px;
+  }
+
+  p {
+    color: #555555;
+    font-size: 16px;
+    line-height: 25px;
+  }
+
+  select {
+    display: block;
+    color: #555555;
+    font-size: 16px;
+    height: 30px;
+    border: none;
+    outline: none;
+  }
+
+  div {
+    padding: 30px 0;
+    width: 350px;
+    display: block;
+
+    input {
+      font-size: 16px;
+      text-align: center;
+      margin: 15px 0;
+      width: 40px;
+      height: 30px;
+      border: 1px solid black;
+    }
+
+    button {
+      font-size: 15px;
+      text-align: center;
+      margin: 15px 0 15px 20px;
+      width: 110px;
+      height: 40px;
+      border: none;
+      border-radius: 3px;
+      cursor: pointer;
+    }
+  }
+
+  @media (max-width: 910px) {
+    img {
+      width: 300px;
+      height: 300px;
+    }
+
+    div {
+      padding: 30px 0;
+      width: 300px;
+    }
+
+    h2 {
+      font-size: 30px;
+      line-height: 40px;
+    }
+
+    p {
+      font-size: 15px;
+    }
+  }
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 0;
+  }
+
+  @media (max-width: 576px) {
+    padding: 20px;
+    width: 85%;
+
+    h2 {
+      font-size: 30px;
+    }
+
+    img {
+      width: 230px;
+      height: 230px;
+    }
+
+    div {
+      padding: 0;
+      width: 90%;
+    }
+  }
+`;
+
+export const BackButton = styled(BsFillArrowLeftSquareFill)`
+  font-size: 35px;
+  position: absolute;
+  right: 20px;
+  top: 0;
+  cursor: pointer;
+
+  @media (max-width: 768px) {
+    top: -20px;
+  }
+`;
