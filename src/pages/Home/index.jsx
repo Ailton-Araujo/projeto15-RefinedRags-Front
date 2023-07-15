40645703893aba8f334be37a3d806da258a578a1
Marcus Dantas <129130733+omarcusdantas@users.noreply.github.com>
2023-07-15 13:47:02 -0300

    feat: add filters and order options for products (#5)
    * feat: filters and order functions for products
    * refactor: separe styled components for better readability

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,33 +1,63 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import { useEffect, useRef, useState } from "react";
 import useProducts from "../../hooks/useProducts";
 import { RotatingLines } from "react-loader-spinner";
 import { Product } from "../../components/index";
+import { TbSortDescending2 } from "react-icons/tb";
+import { HiFilter } from "react-icons/hi";
+import { PageContainer, ButtonsContainer, ButtonSet, OptionsContainer, ProductsContainer } from "./style.js"
+
 
 export default function Home() {
   const { products } = useProducts();
   const [productSets, setProductSets] = useState([]);
   const [currentSet, setCurrentSet] = useState(0);
+  const filterSelected = useRef(null);
+  const orderSelected = useRef(null);
 
   function handleSet(button) {
     if (button === "previous-set") {
-      return (currentSet !== 0 ? setCurrentSet(currentSet-1) : null); 
+      return currentSet !== 0 ? setCurrentSet(currentSet - 1) : null;
     } else if (button === "next-set") {
-      return (currentSet !== productSets.length-1 ? setCurrentSet(currentSet+1) : null);
+      return currentSet !== productSets.length - 1 ? setCurrentSet(currentSet + 1) : null;
     }
     setCurrentSet(parseInt(button));
   }
 
-  function handleProductSets() {
+  function handleProductSets(list) {
     const sets = [];
-    for (let i = 0; i < products.length; i += 6) {
-      sets.push(products.slice(i, i + 6));
+    for (let i = 0; i < list.length; i += 6) {
+      sets.push(list.slice(i, i + 6));
     }
+    setCurrentSet(0);
     setProductSets(sets);
   }
 
+  function orderProducts(list) {
+    let newProductsOrder = [];
+    if (orderSelected.current.value === "rating" || orderSelected === null) {
+      newProductsOrder = list.sort((a, b) => b.rating - a.rating);
+    } else if (orderSelected.current.value === "latest") {
+      newProductsOrder = list.sort((a, b) => b.productId - a.productId);
+    } else if (orderSelected.current.value === "highest-price") {
+      newProductsOrder = list.sort((a, b) => b.price - a.price);
+    } else if (orderSelected.current.value === "lowest-price") {
+      newProductsOrder = list.sort((a, b) => a.price - b.price);
+    }
+    handleProductSets(newProductsOrder);
+  }
+
+  function filterProducts() {
+    let filteredProducts = [];
+    if (filterSelected.current.value === "all" || filterSelected === null) {
+      filteredProducts = products;
+    } else {
+      filteredProducts = products.filter(product => product.type === filterSelected.current.value);
+    }
+    orderProducts(filteredProducts);
+  }
+
   useEffect(() => {
-    handleProductSets();
+    filterProducts();
   }, [products]);
 
   return (
@@ -46,12 +76,33 @@ export default function Home() {
           <p>{`>`}</p>
         </ButtonSet>
       </ButtonsContainer>
+      <OptionsContainer>
+        <div>
+          <label htmlFor="sort-options"><TbSortDescending2 />Sort by :</label>
+          <select id="sort-options" ref={orderSelected} onChange={filterProducts}>
+            <option value="rating">Rating</option>
+            <option value="latest">Latest</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="lowest-price">Lowest Price</option>
+          </select>
+        </div>
+        <div>
+          <label htmlFor="filter-options"><HiFilter />Filter :</label>
+          <select id="filter-options" ref={filterSelected} onChange={filterProducts}>
+            <option value="all">All</option>
+            <option value="shirt">Shirts</option>
+            <option value="long-sleeved shirt">Long-sleeved Shirts</option>
+            <option value="shorts">Shorts</option>
+            <option value="sneakers">Sneakers</option>
+          </select>
+        </div>
+      </OptionsContainer>
       <ProductsContainer>
         {products.length === 0 ? (
           <RotatingLines strokeColor="#ffffff" strokeWidth="4" width="80" />
         ) : (
-          productSets[currentSet]?.map((product, index) => (
-            <li key={index}>
+          productSets[currentSet]?.map((product) => (
+            <li key={product.productId}>
               <Product info={product} />
             </li>
           ))
@@ -60,60 +111,3 @@ export default function Home() {
     </PageContainer>
   );
 }
-
-const PageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  width: 100%;
-  margin-top: 200px;
-`;
-
-const ProductsContainer = styled.ul`
-  padding: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  max-width: 800px;
-  flex-wrap: wrap;
-  gap: 40px;
-  background-color: #81ecff4c;
-
-  @media (max-width: 576px) {
-    gap: 20px;
-    padding: 10px;
-  }
-`;
-
-const ButtonSet = styled.button`
-  background-color: #3b3eff;
-  color: #ffffff;
-  margin-left: 15px;
-  width: 40px;
-  height: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  border-radius: 8px;
-  cursor: pointer;
-  border: none;
-
-  &:nth-child(${(props) => props.$setIndex + 2}) {
-    background-color: #ffffff;
-    color: #555555;
-  }
-
-  p {
-    color: inherit;
-    font-size: 20px;
-  }
-`;
-
-const ButtonsContainer = styled.div`
-  margin-bottom: 40px;
-  display: flex;
-  justify-content: center;
-`;
